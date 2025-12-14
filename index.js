86add6dd59ea7b64093873b434a61f1aff756542
protivaghosh <protivaghosh50@gmail.com>
2025-12-14 07:22:12 +0600

    added to rider patch

--- index.js
+++ index.js
@@ -25,6 +25,34 @@ function generateTrackingId(prefix = "ZAP") {
 app.use(express.json());
 app.use(cors());
 
+const admin = require("firebase-admin");
+
+const serviceAccount = require("./zap-shift-resources-firebase-adminsdk-fbsvc-0a75b5439c.json");
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount)
+});
+
+const verifyFBToken = async(req, res, next) =>{
+  //  console.log('header in the middleware', req.headers.authorization);
+   const token = req.headers.authorization;
+
+   if(!token) {
+    return res.status(401).send({message: 'unauthorize access'});
+   }
+   try{
+  const IdToken= token.split(' ')[1];
+  const decoded = await admin.auth().verifyIdToken(IdToken);
+  console.log('decoded in the token', decoded);
+  req.decoded_email = decoded.email
+   next();
+
+   }
+   catch(err){
+    return res.status(401).send({message:'unauthorize assess' })
+   };
+} 
+
 
 const uri = `mongodb+srv://${process.env.DB_user}:${process.env.DB_Password}@cluster0.1wh8t.mongodb.net/?appName=Cluster0`;
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -42,8 +70,25 @@ async function run() {
     await client.connect();
 
    const db = client.db('zap-shift-resources');
+   const userCollection = db.collection('users');
    const parcelsCollection = db.collection('parcels');
    const paymentCollection = db.collection('payments');
+   const ridersCollection = db.collection('riders');
+
+
+  //  user related api
+  app.post('/user', async(req, res)=>{
+    const user = req.body;
+    user.role = 'user'
+    user.createdAt = new Date();
+    const email = user.email;
+    const userExit = await userCollection.findOne({email})
+         if(userExit){
+          return res.send({message:'user exit'})
+         }
+    const result = await userCollection.insertOne(user);
+    res.send(result);
+  })
 
   //  parcel api
   app.get('/parcels', async(req, res)=>{
@@ -118,10 +163,23 @@ async function run() {
     app.patch('/payment-success',  async(req, res)=>{
       const sessionId = req.query.session_id;
      const session = await stripe.checkout.sessions.retrieve(sessionId);
-     console.log('session retrieve', session);
-     const trackingId = generateTrackingId(); 
 
-     if(session. payment_status === 'paid'){
+    //  console.log('session retrieve', session);
+       const transactionId = session.payment_intent;
+       const query = {transactionId : transactionId}
+
+       const paymentExist = await paymentCollection.findOne(query);
+       if(paymentExist){
+          return res.send({message: 'already exist', transactionId,
+          trackingId : paymentExist.trackingId
+
+          })
+       }
+     
+     const trackingId = generateTrackingId();
+
+
+     if(session.payment_status === 'paid'){
       const id = session.metadata.parcelId;
       const query = { _id: new ObjectId(id) }
       const update = {
@@ -140,7 +198,8 @@ async function run() {
               parcelName : session.metadata.parcelName,
               transactionId : session.payment_intent,
               paymentStatus : session.payment_status,
-              paidAt : new Date()
+              paidAt : new Date(),
+              trackingId: trackingId
              
       }
       if(session.payment_status === 'paid'){
@@ -160,6 +219,74 @@ async function run() {
       res.send({success:false})
 
     })   
+
+    // payment related api
+    app.get('/payments',verifyFBToken, async(req, res)=>{
+      const email = req.query.email
+      const query ={}
+      // console.log(req.headers)
+      if(email){
+        query.customerEmail = email;
+
+        // check email address
+
+        if(email !==  req.decoded_email){
+          return res.status(403).send({massage : 'forbidden access'})
+        }
+      }
+      const cursor = paymentCollection.find(query).sort({paidAt: -1});
+      const result = await cursor.toArray();
+      res.send(result);
+    })
+
+    // riders related api
+  app.get('/riders', async(req, res)=>{
+    const query = {}
+    if(req.query.status){
+      query.status = req.query.status
+    }
+    const cursor = ridersCollection.find(query);
+    const result =  await cursor.toArray();
+    res.send(result);
+  })
+
+    app.post('/riders',  async(req, res)=>{
+    const rider = req.body;
+    rider.status ='pending';
+    rider.createdAt = new Date();
+
+    const result = await ridersCollection.insertOne(rider);
+    res.send(result);
+    
+    })
+
+
+     app.patch('/riders/:id', verifyFBToken, async (req, res) => {
+            const status = req.body.status;
+            const id = req.params.id;
+            const query = { _id: new ObjectId(id) }
+            const updatedDoc = {
+                $set: {
+                    status: status
+                }
+            }
+
+            const result = await ridersCollection.updateOne(query, updatedDoc);
+
+            if (status === 'approved') {
+                const email = req.body.email;
+                const userQuery = { email }
+                const updateUser = {
+                    $set: {
+                        role: 'rider'
+                    }
+                }
+                const userResult = await userCollection.updateOne(userQuery, updateUser);
+            }
+
+            res.send(result);
+        })
+
 
 
     // Send a ping to confirm a successful connection
